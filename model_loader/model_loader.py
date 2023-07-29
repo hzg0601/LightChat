2c4f4396d035a051a82e894386b0ad71c4e9e1e1
hzg0601 <hzg0601@163.com>
2023-07-29 14:02:34 +0800

    update model_loader

--- model_loader/model_loader.py
+++ model_loader/model_loader.py
@@ -11,7 +11,8 @@ from typing import Union,Optional
 from pathlib import Path
 import torch
 
-from .until import logger
+from .until import (logger,
+                    is_ipex_available)
 
 
 class ModelInfo:
@@ -26,6 +27,7 @@ class LoaderInfo:
     use_fast:bool=False #
     peft_name_or_path:Optional[str] = None
     device_map: Optional[str] = "auto" # "auto", "balanced"
+    torch_dtype: Optional[torch.dtype] = torch.float16
 
 
 def recur_download_model(model_name_or_path:str,max_try:int=300) -> Path:
@@ -61,44 +63,76 @@ def recur_download_model(model_name_or_path:str,max_try:int=300) -> Path:
 
 
 class ModelLoader(object):
-    def __init__(model_info:ModelInfo,
-                loader_info:LoaderInfo
+    def __init__(self,
+                 model_info:ModelInfo,
+                loader_info:Optional[LoaderInfo]=None
     ):
         assert not (self.model_info.quantized and self.loader_info.quant_bit), "can not quantize a model that has been quantized"
 
         if loader_info.device == "auto":
             loader_info.device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
-        assert not(not loader_info.device.lower().startswith("cuda") and loader_info.quant_bit),"quantization can only be executed on gpu"
 
-        if (sys.platform == "windows" and loader_info.quant_bit):
-            logger.warning("One have to se compression.py to execute quantization in windows currently, however it is not supported for dispatch model")
+        assert not(not loader_info.device.lower().startswith("cuda") and loader_info.quant_bit),"quantization can only be executed on gpu"
+        
+        model_info.model_name_or_path = recur_download_model(model_info.model_name_or_path)
+        
+        self.model_info = model_info
+
+        if sys.platform == "windows" and loader_info.quant_bit and loader_info.device_map:
+            logger.warning("Can only use compression.py to execute quantization in windows currently, however it is not supported for dispatch model")
             loader_info.device_map = None
-        if not (torch.cuda.is_available() and loader_info.device.lower().startswith("cuda") and torch.cuda.device_count() >=2 ):
-            loader_info.warning("Can only execute dispatching model when setting `loader_info.device=cuda` and the number of gpus greater than 1 ")
+        use_cuda = torch.cuda.is_available() and loader_info.device.lower().startswith("cuda")
+        if use_cuda and (not torch.cuda.device_count() > 2) and loader_info.device_map:
+            loader_info.warning("Can only execute dispatching model when `loader_info.device=cuda` and the number of gpus greater than 1 if use gpu")
             loader_info.device_map = None
+
+        use_xpu = is_ipex_available() and torch.xpu.is_available() and loader_info.device.lower().startswith("xpu")
+        if use_xpu and (not torch.xpu.device_count() > 1) and loader_info.device_map:
+            loader_info.warning("Can only execute dispatching model when `loader_info.device=xpu` and the number of xpus greater than 1 if use xpu")
+            loader_info.device_map = None
+            
+        if use_cuda and (not torch.cuda.is_bf16_supported()) and (loader_info.torch_dtype == torch.bfloat16):
+            logger.warning('Your GPU does not support bfloat16, use float16 instead')
+            loader_info.torch_dtype = torch.float16
+
+        if use_xpu and loader_info.torch_dtype==torch.float16:
+            logger.warning("Intel XPU does not support float16 yet, use bfloat16 instead")
+            loader_info.torch_dtype = torch.bfloat16
 
         if loader_info.peft_name_or_path:
             loader_info.peft_name_or_path = recur_download_model(loader_info.peft_name_or_path)
         
         self.loader_info = loader_info
 
-        
-        model_info.model_name_or_path = recur_download_model(model_info.model_name_or_path)
-        self.model_info = model_info
 
     def load_hf(self):
         """A function that loads a model of `transformers`
         """
         
-        model_config = AutoConfig.from_pretrained(self.model_info.model_name_or_path)
+        model_config = AutoConfig.from_pretrained(self.model_info.model_name_or_path,trust_remote_code=True)
+        kwargs = {}
         if self.model_info.quant_bit and sys.platform != "windows":
-            form bitandbytes import BitsAndBytesConfig
+            from bitsandbytes import BitsAndBytesConfig
             quant_config = BitsAndBytesConfig(load_in_8bit=self.loader_info.quant_bit=8,
-                                            load_in_4bit=self.loader_info.quant_bit=4)
+                                            load_in_4bit=self.loader_info.quant_bit=4,
+                                            llm_int8_threshold=6.0,
+                                            llm_int8_has_fp16_weights=False
+                                            )
+            kwargs["quantization_config"] = quant_config
+        if self.loader_info.device_map:
+            kwargs["device_map"] = self.loader_info.device_map
+        kwargs['torch_dtype'] = self.loader_info.torch_dtype
 
-
-        with init_empty_weights():
-            MODEL = 
-        
-        
+        # with init_empty_weights():
+        try:
+            model = AutoModel.from_config(self.model_info.model_name_or_path,
+                                            config=model_config,
+                                            trust_remote_code=True,
+                                            **kwargs)
+    
+        except NameError:
+            model = AutoModelForCausalLM.from_config(self.model_info.model_name_or_path,
+                                                        config=model_config,
+                                                        trust_remote_code=True,
+                                                        **kwargs)
         
--- utils.py
+++ utils.py
@@ -1,5 +1,17 @@
+'''
+Description: 
+version: 
+Author: huangzg
+LastEditors: huangzg
+Date: 2023-07-28 22:56:02
+LastEditTime: 2023-07-29 12:41:22
+'''
 import logging
 import os
+from packaging import version
+from packaging.version import parse
+import importlib
+import warnings
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)  # set logger level
@@ -9,3 +21,26 @@ formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(l
 ch = logging.StreamHandler()
 ch.setFormatter(formatter)
 logger.addHandler(ch)
+
+
+def is_ipex_available():
+    def get_major_and_minor_from_version(full_version):
+        return str(version.parse(full_version).major) + "." + str(version.parse(full_version).minor)
+
+    _torch_version = importlib.metadata.version("torch")
+    if importlib.util.find_spec("intel_extension_for_pytorch") is None:
+        return False
+    _ipex_version = "N/A"
+    try:
+        _ipex_version = importlib.metadata.version("intel_extension_for_pytorch")
+    except importlib.metadata.PackageNotFoundError:
+        return False
+    torch_major_and_minor = get_major_and_minor_from_version(_torch_version)
+    ipex_major_and_minor = get_major_and_minor_from_version(_ipex_version)
+    if torch_major_and_minor != ipex_major_and_minor:
+        warnings.warn(
+            f"Intel Extension for PyTorch {ipex_major_and_minor} needs to work with PyTorch {ipex_major_and_minor}.*,"
+            f" but PyTorch {_torch_version} is found. Please switch to the matching version and run again."
+        )
+        return False
+    return True
